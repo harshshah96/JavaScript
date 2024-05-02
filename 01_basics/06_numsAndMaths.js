5924b913d598c89ef1921b7a55d38b4347595951
Harsh Shah <shahharsh987656@gmail.com>
2024-05-02 07:48:13 +0000

    math/num

--- 01_basics/06_numsAndMaths.js
+++ 01_basics/06_numsAndMaths.js
@@ -1,12 +1,40 @@
 const score = 400
-console.log(score)
+// console.log(score)
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
 
-console.log(balance.toString().length)
-console.log(balance.toFixed(1))
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
 
 const otherNumber = 11123.8966
 
-console.log(otherNumber.toPrecision(3))
+// console.log(otherNumber.toPrecision(3))
+
+
+const hundereds = 1000000
+
+// console.log(hundereds.toLocaleString('en-IN'))
+
+
+
+// ++++++++++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-1)) // 1
+// console.log(Math.round(4.3)) // 4
+// console.log(Math.round(4.6)) // 5
+// console.log(Math.ceil(4.2)) // 5
+// console.log(Math.floor(4.9)) // 4
+// console.log(Math.min(4,5,2,1,2,4)) // 1
+
+console.log(Math.floor(Math.random()))
+const max=10
+const min = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min)  // important
+
+// console.log( (Math.random(1,2,3,4,5,6) * 10) + 1)
+
+
+
