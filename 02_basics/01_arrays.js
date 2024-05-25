f845e65718f31d3bccc80da513a6c4a3928e6a26
Harsh Shah <shahharsh987656@gmail.com>
2024-05-25 15:28:23 +0000

    arrays/objects

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -20,7 +20,6 @@ myArr.shift()
 const newArr = myArr.join();
 
 // console.log(myArr)
-// console.log(newArr)
 
 // +++++++++++++++++++++++ splice slice +++++++++++++++++++++++++++
 
@@ -33,6 +32,13 @@ console.log("B ", myArr)
 const myn2 = myArr.splice(1,3)
 console.log("C ", myArr)
 console.log(myn2)
+
+ // +++++++++++++++++    NOTES    +++++++++++++++++++++
+
+    // slice(a,b) will print from index a upto index b , excluding the element at b
+    // slice(a,b) will not modify the array
+    // splice(a,b) will print from index a to index b , including the element at b.
+    // splice (a,b) will modify the original array, i.e will remove all element from a to b including a & b index elements.
 
 
 
---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,38 @@
+
+// const  marvelHeros = ["Thor", "Ironman", "Spiderman"];
+// const  dcHeros = ["superman", "flash", "batman"];
+
+
+// Push
+// marvelHeros.push(dcHeros)
+// console.log(marvelHeros[3][1]);
+
+
+// Concatenation
+// const allHeros = marvelHeros.concat(dcHeros);
+// console.log(allHeros);
+
+
+
+// Spread operators (more preferable to merge two arrays)
+// const allHeros2 = [...marvelHeros , ...dcHeros]
+// console.log(allHeros2);
+
+
+// Array flatening 
+// const another_array  = [1, 2, 3 , [ 4,5,6], 7, [6,7,8,9,[1,9,6]]];
+// console.log(another_array);
+// const realAnotherArray = another_array.flat(Infinity);
+// console.log(realAnotherArray);
+
+
+
+let score1=100;
+let score2=200;
+let score3=300;
+console.log(Array.isArray("Hitesh"));
+console.log(Array.from({name: "Hitesh"})); // Interseting /IMPORTANT
+console.log(Array.of(score1,score2,score3));
+
+
+
