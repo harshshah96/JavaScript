523cfbb2de5b5750c57db2e4516b99d99e71b352
Harsh Shah <shahharsh987656@gmail.com>
2024-05-29 09:38:04 +0000

    for/while

---
+++ 05_itterations/one.js
@@ -0,0 +1,20 @@
+
+// for (let i = 1; i <=10; i++) {
+//     const element = i;
+//     if(i==5){
+//         console.log("5 is best Number");
+//     }
+//     console.log(element);
+    
+// }
+
+// console.log(i);  // ERROR
+
+for (let i = 0; i <=10; i++) {
+    // console.log(`   Outer Loop value: ${i}`)
+    for (let j = 0; j <=10; j++) {
+
+        console.log(`${(i*10 + j)}`)
+    }
+    
+}
