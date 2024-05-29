afe328a4238f1bfe33d038ded0868cebadb74b9b
Harsh Shah <shahharsh987656@gmail.com>
2024-05-29 15:14:41 +0000

    loops

---
+++ 05_itterations/five.js
@@ -0,0 +1,40 @@
+const coding = ["Js","Ruby", "py", "java","cpp"];
+
+// coding.forEach(function (item){
+//     console.log(item);
+// });
+
+
+coding.forEach((item)=>{
+    // console.log(item);
+})
+
+function printMe(item){
+    console.log(item);
+}
+
+// coding.forEach(printMe);
+
+coding.forEach((item, index, arr)=>{
+    // console.log(item, index, arr);
+})
+
+const myCoding = [
+    {
+        languageName: "JavaScript",
+        languageFileName : "js"
+    },
+    {
+        languageName: "C++",
+        languageFileName : "cpp"
+    },
+    {
+        languageName: "python",
+        languageFileName : "py"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.languageName, ":- ", item.languageFileName);
+})
+
